bc93305a10d1e163567d4f67ebd83999e6e229e1
Nash Brancic <nashfft@gmail.com>
2019-02-06 00:48:01 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,12 +12,10 @@ end
 
 def long_planeteer_calls(array)
   calls_short = ["wind", "fire"]
-  array.any? {|spell| spell.length >=4
-  
-  
-  # code an argument here
-  # Your code here
+  array.any? {|spell| spell.length >=4}
 end
+end
+
 
 def find_the_cheese# code an argument here
   # the array below is here to help
