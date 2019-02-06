2578f79579b7a1e71c22e5182ca475f3b8a34a4b
Nash Brancic <nashfft@gmail.com>
2019-02-06 00:40:14 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,7 +10,10 @@ def summon_captain_planet(planeteer_calls)
   end
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(array)
+  
+  
+  # code an argument here
   # Your code here
 end
 
