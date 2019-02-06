f126c7cc4bebe61d7ab9af6ce202486cea6f0676
Nash Brancic <nashfft@gmail.com>
2019-02-06 01:06:27 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,10 +15,10 @@ def long_planeteer_calls(array)
 end
 
 def find_the_cheese(array)
-  cheese_types == ["cheddar", "gouda", "camembert"]
-    if array.find.include?(cheese_types)
+  array = ["cheddar", "gouda", "camembert"]
+    if array.find.include?("cheddar")
     "#{cheese}"
   else
     nil
   end
-end 
+end
